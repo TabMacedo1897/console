675fcae2cee94986a9a472c306c0c230eb5e1bf9
Professora Tábata Macedo <106038097+TabMacedo1897@users.noreply.github.com>
2023-01-10 12:07:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var nome = "Maria Heloisa";
+console.log(nome)
+
+var sobrenome = "Figueira"
+console.log(sobrenome)
+
+var nomeCompleto = nome + sobrenome
+console.log(nomeCompleto)
+
+var idade = 10;
+console.log(idade)
+
+var comprimentoNomeCompleto = nomeCompleto.length;
+console.log(comprimentoNomeCompleto)
+
+var primeiroNome = "Oi, eu sou a Maria Heloisa"
+console.log(primeiroNome)
+
+var sub = primeiroNome.replace("Maria Heloisa", "Tábata")
+console.log(sub)
+
+var capitalNome = "rocky"
+console.log(capitalNome)
+
+var facaMaiusculo = capitalNome.toUpperCase()
+console.log(facaMaiusculo)
+
+var raizQuadrada = Math.sqrt(16)
+console.log(raizQuadrada)
+
+var maximumNumber = Math.max(10, 15)
+console.log(maximumNumber)
